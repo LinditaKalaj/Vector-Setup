76c8dcd059fe8f6305363dba6fce71ae6c9d60ab
Lindita <linditakalaj@gmail.com>
2023-01-04 20:31:22 -0500

    Added validation for form input

--- gui.py
+++ gui.py
@@ -1,5 +1,4 @@
-from PIL import Image
-
+import re
 
 import customtkinter as cTK
 
@@ -24,12 +23,10 @@ class App(cTK.CTk):
         self.sidebar_frame.grid_columnconfigure(1, weight=1)
         self.sidebar_frame.grid_columnconfigure(2, weight=1)
 
-
-
         self.header = cTK.CTkLabel(self.sidebar_frame,
-                                      text="Welcome to ezVector setup!\n"
-                                           "Fill in this form to generate "
-                                           "a certificate for your robot")
+                                   text="Welcome to ezVector setup!\n"
+                                        "Fill in this form to generate "
+                                        "a certificate for your robot")
         self.header.grid(row=0, column=0, columnspan=3, padx=20, pady=20)
 
         self.nameLabel = cTK.CTkLabel(master=self, text="Vector's Name: ")
@@ -37,32 +34,6 @@ class App(cTK.CTk):
 
         self.nameEntry = cTK.CTkEntry(master=self, width=200, placeholder_text="ex. C5W2")
         self.nameEntry.grid(row=1, column=1, padx=20, pady=20)
-
-        self.img = cTK.CTkImage(Image.open("./img/informationButton.png"), size=(30, 30))
-        self.img2 = cTK.CTkImage(Image.open("./img/informationButton2.png"), size=(30, 30))
-
-        self.qMark1 = cTK.CTkLabel(master=self, image=self.img, text="")
-        self.qMark1.grid(row=1, column=2, columnspan=1)
-
-        def _press(evt=None):
-            self.pressed = self.qMark1
-            self.qMark1.configure(image=self.img2)
-
-        self.qMark1.bind("<ButtonPress-1>", _press)
-
-        def _leave(evt=None):
-            if self.pressed is self.qMark1:
-                self.qMark1.configure(image=self.img)
-                self.pressed = None
-
-        self.qMark1.bind('<Leave>', _leave)
-
-        def _release(evt=None):
-            # Should only do this if we didn't move the mouse away
-            if self.pressed is self.qMark1:
-                _leave(evt)
-
-        self.qMark1.bind("<ButtonRelease-1>", _release)
 
         self.ipLabel = cTK.CTkLabel(master=self, text="Vector's IP: ")
         self.ipLabel.grid(row=2, column=0, padx=20, pady=20)
@@ -88,8 +59,10 @@ class App(cTK.CTk):
         self.passWordEntry = cTK.CTkEntry(master=self, width=200, placeholder_text="***********", show="*")
         self.passWordEntry.grid(row=5, column=1, padx=20, pady=20)
 
-        self.generateButton = cTK.CTkButton(master=self, text="Generate Config Document")
+        self.generateButton = cTK.CTkButton(master=self, text="Generate Config Document", command=self.send_to_setup_tools)
         self.generateButton.grid(row=6, column=0, columnspan=3, padx=20, pady=(20, 20))
+
+        self.set_bindings()
 
     def configure_style(self):
         self.configure(background='#303030')
@@ -105,3 +78,55 @@ class App(cTK.CTk):
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
         self.grid_columnconfigure(2, weight=1)
+
+    def set_bindings(self):
+        self.nameEntry.bind("<Key>", lambda event: self.turn_back_fg(self.nameEntry))
+        self.ipEntry.bind("<Key>", lambda event: self.turn_back_fg(self.ipEntry))
+        self.snEntry.bind("<Key>", lambda event: self.turn_back_fg(self.snEntry))
+        self.snEntry.bind("<Key>", lambda event: self.turn_back_fg(self.emailEntry))
+
+    def send_to_setup_tools(self):
+        name = self.validate_name(self.nameEntry)
+        ip = self.validate_ip(self.ipEntry)
+        serial = self.validate_sn(self.snEntry)
+        email = self.validate_email(self.emailEntry)
+        password = self.passWordEntry.get()
+
+    def validate_name(self, name_entry):
+        name = name_entry.get()
+        if len(name) == 4:
+            name = "Vector-{}".format(name.upper())
+        if re.match("[Vv]ector.[A-Za-z0-9]{4}", name):
+            name = "V{}-{}".format(name[1:-5], name[-4:].upper())
+        if re.match("Vector-[A-Z0-9]{4}", name):
+            return name
+        else:
+            self.highlight_error(name_entry)
+
+    def validate_ip(self, ip_entry):
+        ip = ip_entry.get()
+        if re.match("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", ip):
+            return ip
+        else:
+            self.highlight_error(ip_entry)
+
+    def validate_sn(self, sn_entry):
+        sn = sn_entry.get()
+        if len(sn) == 8:
+            return sn
+        else:
+            self.highlight_error(sn_entry)
+
+    def validate_email(self, email_entry):
+        email = email_entry.get()
+        if re.match("r^\S+@\S+\.\S+$", email):
+            return email
+        else:
+            self.highlight_error(email_entry)
+
+    def highlight_error(self, entry):
+        entry.focus_force()
+        entry.configure(fg_color="tomato4")
+
+    def turn_back_fg(self, entry):
+        entry.configure(fg_color="gray21")
