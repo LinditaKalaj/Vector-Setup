8328b5eb0ab885d3e4d6c98ad878373aabfae230
lindita <linditakalaj@gmail.com>
2023-04-17 16:11:15 -0400

    added additional unit tests to email validation

--- test/validations/emailValidation.py
+++ test/validations/emailValidation.py
@@ -27,6 +27,14 @@ class EmailValidation(unittest.TestCase):
         self.test_gui.emailEntry.insert(0, "nodomain@.com")
         self.assertIsNone(self.test_gui.validate_email())
 
+    def test_invalid_domain_4(self):
+        self.test_gui.emailEntry.insert(0, "space@na sa.com")
+        self.assertIsNone(self.test_gui.validate_email())
+
+    def test_invalid_domain_5(self):
+        self.test_gui.emailEntry.insert(0, "newline@new\nsunday.com")
+        self.assertIsNone(self.test_gui.validate_email())
+
     def test_invalid_top_lvl_domain(self):
         self.test_gui.emailEntry.insert(0, "anotheremail@google.")
         self.assertIsNone(self.test_gui.validate_email())
@@ -37,6 +45,18 @@ class EmailValidation(unittest.TestCase):
 
     def test_invalid_top_lvl_domain_3(self):
         self.test_gui.emailEntry.insert(0, "eww@ghotmail")
+        self.assertIsNone(self.test_gui.validate_email())
+
+    def test_invalid_top_lvl_domain_4(self):
+        self.test_gui.emailEntry.insert(0, "equals@lake.==")
+        self.assertIsNone(self.test_gui.validate_email())
+
+    def test_invalid_top_lvl_domain_5(self):
+        self.test_gui.emailEntry.insert(0, "kevin@spacey.c o")
+        self.assertIsNone(self.test_gui.validate_email())
+
+    def test_invalid_top_lvl_domain_6(self):
+        self.test_gui.emailEntry.insert(0, "nonewline@gmail.c\n")
         self.assertIsNone(self.test_gui.validate_email())
 
     def test_top_lvl_domain(self):
@@ -57,6 +77,34 @@ class EmailValidation(unittest.TestCase):
 
     def test_invalid_local_name_3(self):
         self.test_gui.emailEntry.insert(0, "hehe~acha@gmail.com")
+        self.assertIsNone(self.test_gui.validate_email())
+
+    def test_invalid_local_name_4(self):
+        self.test_gui.emailEntry.insert(0, "lalala\n@gmail.com")
+        self.assertIsNone(self.test_gui.validate_email())
+
+    def test_invalid_local_name_5(self):
+        self.test_gui.emailEntry.insert(0, "spacing @nasa.com")
+        self.assertIsNone(self.test_gui.validate_email())
+
+    def test_invalid_local_name_6(self):
+        self.test_gui.emailEntry.insert(0, "wherego.com")
+        self.assertIsNone(self.test_gui.validate_email())
+
+    def test_at_sign(self):
+        self.test_gui.emailEntry.insert(0, "imampedup.com")
+        self.assertIsNone(self.test_gui.validate_email())
+
+    def test_at_sign_2(self):
+        self.test_gui.emailEntry.insert(0, "imamped&up.com")
+        self.assertIsNone(self.test_gui.validate_email())
+
+    def test_no_special_chars(self):
+        self.test_gui.emailEntry.insert(0, "wowowewagmailcom")
+        self.assertIsNone(self.test_gui.validate_email())
+
+    def test_blank(self):
+        self.test_gui.emailEntry.insert(0, "")
         self.assertIsNone(self.test_gui.validate_email())
 
 
